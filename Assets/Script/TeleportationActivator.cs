e22ba1e2a65433a2e681ba552da017e9ff08bae0
aledifon <aledifon89@gmail.com>
2026-02-14 20:16:47 +0100

    Implemented Ray Interactor for UI interaction + XR Intearction Group to handle diferent interactors

--- Assets/Script/TeleportationActivator.cs
+++ Assets/Script/TeleportationActivator.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit.UI;
 
 public class TeleportationActivator : MonoBehaviour
 {
     public XRRayInteractor teleportInteractor;
+    public XRRayInteractor rayInteractor;
     public InputActionProperty teleportActivatorAction;
 
 
@@ -12,8 +14,13 @@ public class TeleportationActivator : MonoBehaviour
     void Start()
     {
         teleportInteractor.gameObject.SetActive(false);
-
         teleportActivatorAction.action.performed += Action_performed;
+        // The UIHoverEventArgs sent from uiHoverEntered Event are ignored and
+        // the DisableTeleportRay is called through a lambda expression.
+        rayInteractor.uiHoverEntered.AddListener(x => DisableTeleportRay());
+
+        // Alternative way in case DisableTeleportRay has the same sign
+        //rayInteractor.uiHoverEntered.AddListener(DisableTeleportRay); 
     }
 
     private void OnDisable()
@@ -21,8 +28,24 @@ public class TeleportationActivator : MonoBehaviour
         teleportActivatorAction.action.performed -= Action_performed;
     }
 
+    public void DisableTeleportRay()
+    {
+        teleportInteractor.gameObject.SetActive(false);
+    }
+
+    // Alternative way in case we make it match with the sign of uiHoverEntered
+    //public void DisableTeleportRay(UIHoverEventArgs args)
+    //{
+    //    teleportInteractor.gameObject.SetActive(false);
+    //}
+
     private void Action_performed(InputAction.CallbackContext obj)
     {
+        // In case there is a Ray Interactor over an UI object then
+        // the Teleport Interactor won't be enabled.
+        if (rayInteractor && rayInteractor.IsOverUIGameObject())
+            return;
+
         teleportInteractor.gameObject.SetActive(true);
     }
 

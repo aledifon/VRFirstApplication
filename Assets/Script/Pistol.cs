00553f0478eff65a903d7e5c0c056621c8e7b4f0
aledifon <aledifon89@gmail.com>
2026-02-14 12:09:22 +0100

    Migrated VR Project from Unity 6.3 LTS to 2022.3 LTS due to stutter issues. Built the new Scene based from the VR template.

---
+++ Assets/Script/Pistol.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private float _bulletSpeed = 20f;
+    [SerializeField] private float _bulletLifeTime = 5f;   // secs before bullet is destroyed
+
+    [SerializeField] private AudioClip _clip;
+    private AudioSource _source;
+
+    void Start()
+    {
+       _source = GetComponent<AudioSource>();
+    }
+
+    public void FireBullet()
+    {
+        GameObject bullet = Instantiate(_bulletPrefab, _firePoint.position, _firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+        _source.PlayOneShot(_clip);
+
+        if(rb != null)
+            rb.velocity = _firePoint.forward * _bulletSpeed;
+
+        // Destroyt the bullet after a set time
+        Destroy(bullet, _bulletLifeTime);
+    }
+}
